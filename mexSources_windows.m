90680210e365b6dc7c2c726fbcfdab9452af5101
hlei-ziyan <hlei.ziyan@gmail.com>
2019-05-13 12:40:04 +0800

    Update mexSources_windows.m

--- mexSources_windows.m
+++ mexSources_windows.m
@@ -9,4 +9,3 @@ setenv('CUDA_BIN_PATH','C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\
 setenv('CUDA_LIB_PATH', 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\lib');
 
 mexcuda -v -lcublas.lib mexSphericalConvolution.cu nnsphconv.cu
-mexcuda -v -lcublas.lib mexFastSum.cu
